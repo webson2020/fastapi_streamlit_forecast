fc2adb4ceaa97f39f28ae0b80aba1b4267870630
webson2020 <nrossettiossa@gmail.com>
2024-03-12 18:42:03 -0300

    first commit

---
+++ frontend/homepage.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+
+
+@st.cache_data
+def get_data():
+    data = pd.read_csv('DailyDelhiClimateTrain.csv')
+    data.columns = ['date', 'temperature', 'humidity', 'wind_speed', 'pressure']
+    return data
+
+st.title('Homepage')
+data = get_data()
+weather_variable = st.selectbox('Weather_variable', data.columns[1:])
+st.line_chart(data.set_index('date')[weather_variable])
+
+
